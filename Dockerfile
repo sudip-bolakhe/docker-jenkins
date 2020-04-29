46aa6c8740ebaef08d0de343dbb9180c82691ad0
Sudip Bolakhe <sudipbolakhe08@gmail.com>
2020-04-29 15:18:21 +0545

    Fix the path to docker jar

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM java:8
 EXPOSE 8080
 VOLUME /usr/docker-demo
-COPY  ./build/libs/docker-0.1.jar ./
+COPY  ./build/libs/docker-jenkins-0.1.jar ./
 ENTRYPOINT ["java", "-jar", "docker-0.1.jar"]
--- src/main/java/com/sudip/docker/DockerJenkinsApplication.java
+++ src/main/java/com/sudip/docker/DockerJenkinsApplication.java
@@ -2,9 +2,10 @@ package com.sudip.docker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DockerJenkinsApplication {
+public class DockerJenkinsApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DockerJenkinsApplication.class, args);
